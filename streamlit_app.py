fb97954ee53f4728554c383ead10a8e702adeaf4
ANDRES CIFUENTES <84190781+andrescr3085@users.noreply.github.com>
2023-04-04 09:13:25 -0500

    Update streamlit_app.py
    Added items

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,6 +4,15 @@ import streamlit as st
 st.title('My Parents New Healthy Diner')
 
 st.header('Breakfast Menu')
-st.text('Omega 3 & Blueberry Oatmeal')
-st.text('Kale, Spinach & Rocket Smoothie')
-st.text('Hard-Bolied Free-Range Egg')
+
+st.text('🥣 Omega 3 & Blueberry Oatmeal')
+
+st.text('🥗 Kale, Spinach & Rocket Smoothie')
+
+st.text('🐔 Hard-Bolied Free-Range Egg')
+
+st.text('🥑🍞 Avocado Toast')
+
+st.header('Build Your Own Fruit Smoothie)
+
+
