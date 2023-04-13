563a8b6c934c65d5f4117f0746e9b5e0d8151c56
ANDRES CIFUENTES <84190781+andrescr3085@users.noreply.github.com>
2023-04-13 10:27:24 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -29,7 +29,7 @@ st.dataframe(fruits_to_show)
 
 
 fruityvice_response = rq.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response)
+st.text(fruityvice_response)
 
 
 
