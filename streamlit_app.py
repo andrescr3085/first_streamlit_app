fa896b19d385cb73977fdc0f053d3a03af534367
ANDRES CIFUENTES <84190781+andrescr3085@users.noreply.github.com>
2023-04-14 10:37:30 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,6 +3,7 @@ import streamlit as st
 import pandas as pd
 import requests as rq
 import snowflake.connector
+from urllib.error import URLError
 
 st.title('My Parents New Healthy Diner')
 
@@ -30,15 +31,21 @@ st.dataframe(fruits_to_show)
 
 st.header("Fruityvice Fruit Advice!")
 
-fruit_choice = st.text_input('What fruit would you like information about?','Kiwi')
-st.write('The user entered ', fruit_choice)
+try:
+  fruit_choice = st.text_input('What fruit would you like information about?','Kiwi')
+  if not fruit_choice:
+    st.error('Please select a fruit to get information.')
+  else:
+    fruityvice_response = rq.get("https://fruityvice.com/api/fruit/" + fruit_choice)
 
-fruityvice_response = rq.get("https://fruityvice.com/api/fruit/" + fruit_choice)
+    # write your own comment -what does the next line do? 
+    fruityvice_normalized = pd.json_normalize(fruityvice_response.json())
+    # write your own comment - what does this do?
+    st.dataframe(fruityvice_normalized)
+    
+except URLError as e:
+    st.error()
 
-# write your own comment -what does the next line do? 
-fruityvice_normalized = pd.json_normalize(fruityvice_response.json())
-# write your own comment - what does this do?
-st.dataframe(fruityvice_normalized)
 
 
 add_my_fruit = st.text_input('What fruit would you like information about?','jackfruit')
